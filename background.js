c71d70bbbe6617f1c8e89220450bc9e08208cfe5
Zaibo Wang <zaibowang@gmail.com>
2018-07-02 18:28:35 -0400

    Create background timer

--- background.js
+++ background.js
@@ -1,14 +1,34 @@
 'use strict';
 
+class DefaultDict {
+  constructor(defaultVal) {
+    return new Proxy({}, {
+      get: (target, name) => name in target ? target[name] : defaultVal
+    })
+  }
+}
+
+const allTimes = new DefaultDict(0);
+
+function parseHostname(url){
+  const parser = document.createElement("a");
+  parser.href = url;
+  console.log("parser")
+  console.log(parser.hostname);
+  return parser.hostname;
+}
+
 chrome.runtime.onInstalled.addListener(
   function() {
     setInterval(() => {
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         tabs.forEach(tab => {
-          chrome.tabs.sendMessage(tab.id, {time: "1", url: tab.url}, function(response) {
-            console.log("Foo");
+          const hostname = parseHostname(tab.url);
+          allTimes[hostname] += 5;
+          chrome.tabs.sendMessage(tab.id, {time: allTimes[hostname]}, function(response) {
+            a = 1
           });
         });
       });
-    }, 1000);
+    }, 5000);
   });
--- content.js
+++ content.js
@@ -1,7 +1,3 @@
-if (window == top) {
-  window.addEventListener('keyup', doKeyPress, false); //add the keyboard handler
-}
-
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.signal == "pause"){
